b450bacafebfe7334d6b38a14ef0c908f1e4aa24
Gonzalo Gamez <takosaga@gmail.com>
2019-10-22 14:26:54 -0500

    Finished

--- app/src/main/java/com/example/githubpractice/MainActivity.java
+++ app/src/main/java/com/example/githubpractice/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.githubpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.clickMeButton);
+
+    }
+
+    public void clickMePress (View view){
+        TextView textView = findViewById(R.id.textViewHello);
+
+        EditText namedEditText = findViewById(R.id.editText);
+
+        textView.setText("Hello " + namedEditText.getText().toString());
     }
 }
